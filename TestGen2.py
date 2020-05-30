fcee557d1fc959c16235b4f82af5bbd417fd3c79
kms040411 <kms040411@kaist.ac.kr>
2020-05-30 15:57:04 +0900

    TestGen v2

--- TestGen2.py
+++ TestGen2.py
@@ -2,13 +2,12 @@ import astor
 import ast
 import sys
 import copy
-import random
 
 class Func_info():
-    def __init__(self, name, args, pointer):
+    def __init__(self, name, args):
         self.name = name
         self.args = args
-        self.pointer = pointer
+        self.predicates = [None] # Do not use index 0
 
 class Predicate_info():
     def __init__(self, num, predicate, precedent):
@@ -16,32 +15,44 @@ class Predicate_info():
         self.predicate = predicate
         self.precedent = precedent
 
+class Answer_info():
+    def __init__(self, func_num, arg, target_predicate):
+        self.func_num = func_num
+        self.arg = arg
+        self.target_predicate = target_predicate
+
 class TestGen():
     config = dict()
     functions = list() # A list of Func_info
-    predicates = [None] # Do not use index 0
-    original_ast = None
+    file_ast = None
     precedent_stack = list()
+    constant_stack = list()
+    constant_num_stack = list()
 
-    def __init__(self, K = 1):
+    def __init__(self, K = 0, DEPTH = 100):
         self.config["K"] = K
+        self.config["DEPTH"] = DEPTH
         return
-
+    
     def do(self, name):
         self.file_input(name)
         self.init_log_file()
         self.modify_ast()
         self.file_output()
-        self.gen_test_suite(0)
+        for i in range(self.current_func + 1):
+            self.gen_test_suite(i)
+        print("end of the program")
+        return
+    
+    # Read the file
+    def file_input(self, name):
+        self.file_ast = astor.parse_file(name)
         return
 
-    # Print help information
-    def help_function(self):
-        pass
-    
-    # Read the file, return ast
-    def file_input(self, name):
-        self.original_ast = astor.parse_file(name)
+    def file_output(self):
+        f = open("modified_code.py", "w")
+        f.write(astor.to_source(self.file_ast))
+        f.close()
         return
 
     def init_log_file(self):
@@ -49,66 +60,75 @@ class TestGen():
         f.write("")
         f.close()
         return
-    
-    def file_output(self):
-        f = open("modified_code.py", "w")
-        f.write(astor.to_source(self.original_ast))
-        f.close()
-        return
 
     def register_function(self, node):
         name = node.name
-        pointer = node
         arg_num = len(node.args.args)
         args = list()
         for i in range(arg_num):
             args.append(node.args.args[i].arg)
-        self.functions.append(Func_info(name, args, pointer))
+        self.functions.append(Func_info(name, args))
         return
 
     def register_predicate(self, node):
-        predicate = node.test
-        predicate_num = 0
-        precedent = copy.deepcopy(self.precedent_stack)
-        #print(precedent)
-        if not isinstance(predicate, ast.Compare):
-            print("it is not compare")
-            raise Exception()
+        predicate = None
+        if not isinstance(node, ast.For):
+            predicate = node.test
         else:
-            predicate_num = len(self.predicates)
-            self.predicates.append(Predicate_info(predicate_num, predicate, precedent))
-            self.precedent_stack.append(predicate_num)
+            predicate = ast.Compare(left=node.target, ops=[ast.LtE], right=node.iter)
+        predicate_num = len(self.functions[self.current_func].predicates)
+        precedent = copy.deepcopy(self.precedent_stack)
+        self.functions[self.current_func].predicates.append(Predicate_info(predicate_num, predicate, precedent))
+        self.precedent_stack.append(predicate_num)
         return predicate_num
+        
 
     def add_helperfunc(self, num, node):
+        args_char = list()
+        args_char = args_char + self.functions[self.current_func].args
+        args_char = args_char + self.constant_stack
+
+        args0 = [ast.Constant(value=num, kind=None), ast.Constant(value=0, kind=None)]
+        args1 = [ast.Constant(value=num, kind=None), ast.Constant(value=1, kind=None)]
+        char_list = list()
+        for item in args_char:
+            char_list.append(item)
+        args0.append(ast.parse(str(char_list), mode='eval'))
+        args1.append(ast.parse(str(char_list), mode='eval'))
+        for item in args_char:
+            args0.append(ast.Name(id = item))
+            args1.append(ast.Name(id = item))
+            
         node.body.insert(0, ast.Expr(value=ast.Call(func=ast.Attribute(value=ast.Name(id=sys.argv[0][:-3]), attr='check_branch'),
-                            args=[ast.Constant(value=num, kind=None), ast.Constant(value=1, kind=None)],
+                            args=args0,
                             keywords=[])))
         node.orelse.insert(0, ast.Expr(value=ast.Call(func=ast.Attribute(value=ast.Name(id=sys.argv[0][:-3]), attr='check_branch'),
-                            args=[ast.Constant(value=num, kind=None), ast.Constant(value=0, kind=None)],
+                            args=args1,
                             keywords=[])))
         return
 
     # Read the ast, analyze predicates, add helper functions to ast
     def modify_ast(self):
+        self.current_func = -1
         def iter_ast(node):
             if isinstance(node, list):
-                #print("list")
+                self.constant_num_stack.append(0)
                 for item in node:
                     iter_ast(item)
+                num = self.constant_num_stack.pop()
+                for i in range(num):
+                    self.constant_stack.pop()
             elif isinstance(node, ast.FunctionDef):
-                #print("function_def")
+                self.current_func = self.current_func + 1
                 self.register_function(node)
                 iter_ast(node.body)
             elif isinstance(node, ast.If):
-                #print("if")
                 predicate_num = self.register_predicate(node)
                 self.add_helperfunc(predicate_num, node)
                 iter_ast(node.body)
                 iter_ast(node.orelse)
                 self.precedent_stack.pop()
             elif isinstance(node, ast.While):
-                #print("while")
                 predicate_num = self.register_predicate(node)
                 self.add_helperfunc(predicate_num, node)
                 iter_ast(node.body)
@@ -120,41 +140,55 @@ class TestGen():
                 iter_ast(node.body)
                 iter_ast(node.orelse)
                 self.precedent_stack.pop()
+            elif isinstance(node, ast.Assign):
+                target = node.targets[0].id
+                self.constant_stack.append(target)
+                self.constant_num_stack.append(self.constant_num_stack.pop() + 1)
             elif isinstance(node, ast.AST):
-                #print("root or normal node")
                 if hasattr(node, "body"):
                     iter_ast(node.body)
                 return
             return
-        self.original_ast.body.insert(0, ast.Import(names=[ast.alias(name=sys.argv[0][:-3], asname=None)]))
-        iter_ast(self.original_ast)
+        self.file_ast.body.insert(0, ast.Import(names=[ast.alias(name=sys.argv[0][:-3], asname=None)]))
+        iter_ast(self.file_ast)
         return
 
     # Write test code
     def gen_test_suite(self, func_num:int):
-        arg = self.init_generate(func_num)
-        for i in range(1, len(self.predicates)):
+        function = self.functions[func_num]
+        predicate = function.predicates
+        f = open("search_result.txt", "w")
+        for i in range(1, len(function.predicates)):
             target_predicate = i
-            current_predicate = self.get_current_predicate(func_num, arg, target_predicate)
-            print("current predicate : " + str(current_predicate))
             print("target predicate : " + str(target_predicate))
-            print("current arg : " + str(arg))
-            arg = self.avm_generate(func_num, current_predicate, target_predicate, arg)
-            while current_predicate != target_predicate:
-                arg = self.avm_generate(func_num, current_predicate, target_predicate, arg)
-                current_predicate = self.get_current_predicate(func_num, arg, target_predicate)
-            print(arg)
-            return
-    
+            arg = self.init_generate(func_num)
+            arg = self.avm_generate(func_num, target_predicate, 1, arg)
+            if arg is None:
+                f.write(str(i) + "T : -\n")
+                print(str(i) + "T : -")
+            else:
+                f.write(str(i) + "T : " + str(arg) + "\n")
+                print(str(i) + "T : " + str(arg))
+            arg = self.init_generate(func_num)
+            arg = self.avm_generate(func_num, target_predicate, 0, arg)
+            if arg is None:
+                f.write(str(i) + "F : -\n")
+                print(str(i) + "F : -")
+            else:
+                f.write(str(i) + "F : " + str(arg) + "\n")
+                print(str(i) + "F : " + str(arg))
+        f.close()
+
     def get_current_predicate(self, func_num:int, args:dict, target_predicate:int):
         predicates = self.execute_test_suite(func_num, args)
-        target_precedent = self.predicates[target_predicate].precedent
+        target_precedent = self.functions[func_num].predicates[target_predicate].precedent
         current_predicate = 1
         for precedent in target_precedent:
             if precedent in predicates:
                 current_predicate = precedent
         return current_predicate
 
+    # Generate 0 args
     def init_generate(self, func_num:int):
         arg_number = len(self.functions[func_num].args)
         arg_list = dict()
@@ -162,28 +196,21 @@ class TestGen():
             arg_list[self.functions[func_num].args[i]] = 0
         return arg_list
 
-    def random_generate(self, func_num:int, start=0, end=10):
+    # Run AVM to find args
+    def avm_generate(self, func_num:int, target_predicate_num:int, target_predicate_option:int, init_arg:dict):
         arg_number = len(self.functions[func_num].args)
-        arg_list = dict()
-        for i in range(arg_number):
-            ran_int = random.randrange(start, end)
-            arg_list.append[self.functions[funct_num].args[i]] = ran_int
-        return arg_list
-
-    def avm_generate(self, func_num:int, predicate_num:int, target_predicate_num:int, init_arg:dict):
-        arg_number = len(self.functions[func_num].args)
-        arg_flag = 1 # 0 ~ (arg_number - 1)
+        arg_flag = 0 # 0 ~ (arg_number - 1)
         arg = copy.deepcopy(init_arg)
-        while True: 
+        for i in range(self.config["DEPTH"] * arg_number):
             # exploratory move
             arg_letter = self.functions[func_num].args[arg_flag]
             target_arg = arg[arg_letter]
             # case 1 : +
             arg[arg_letter] = target_arg + 1
-            fitness_1 = self.calc_fitness(predicate_num, target_predicate_num, arg)
+            fitness_1 = self.calc_fitness(func_num, target_predicate_num, arg)
             # case 2 : -
             arg[arg_letter] = target_arg - 1
-            fitness_2 = self.calc_fitness(predicate_num, target_predicate_num, arg)
+            fitness_2 = self.calc_fitness(func_num, target_predicate_num, arg)
 
             # pattern move
             arg[arg_letter] = target_arg
@@ -193,37 +220,57 @@ class TestGen():
             if fitness_1 < fitness_2 : # +
                 fitness = fitness_1
                 old_fitness = fitness_1
-                while True:
+                for i in range(self.config["DEPTH"]):
                     arg[arg_letter] = arg[arg_letter] + delta
                     old_fitness = fitness
-                    fitness = self.calc_fitness(predicate_num, target_predicate_num, arg)
+                    fitness = self.calc_fitness(func_num, target_predicate_num, arg)
                     delta = delta * 2
-                    #print("fitness : " + str(fitness) + ", old_fitness : " + str(old_fitness))
-                    if fitness >= old_fitness:
+                    if target_predicate_option == 1 and fitness >= old_fitness:
+                        break
+                    elif target_predicate_option == 0 and fitness <= old_fitness:
                         break
                 arg[arg_letter] = arg[arg_letter] - delta / 4    
             elif fitness_1 > fitness_2 : # -
                 fitness = fitness_1
                 old_fitness = fitness_1
-                while True:
+                for i in range(self.config["DEPTH"]):
                     arg[arg_letter] = arg[arg_letter] - delta
                     old_fitness = fitness
-                    fitness = self.calc_fitness(predicate_num, target_predicate_num, arg)
+                    fitness = self.calc_fitness(func_num, target_predicate_num, arg)
                     delta = delta * 2
-                    #print("fitness : " + str(fitness) + ", old_fitness : " + str(old_fitness))
-                    if fitness >= old_fitness:
+                    if target_predicate_option == 1 and fitness >= old_fitness:
+                        break
+                    elif target_predicate_option == 0 and fitness <= old_fitness:
                         break
                 arg[arg_letter] = arg[arg_letter] + delta / 4   
             else:
                 old_fitness = fitness_1
-            if old_fitness <= 0:
-                #print("fitness : " + str(old_fitness))
+            if target_predicate_option == 1 and old_fitness <= 0:
+                return arg
+            elif target_predicate_option == 0 and old_fitness >= 0:
                 return arg
             arg_flag = (arg_flag + 1) % arg_number
+        # Cannot find answer
+        return None
 
     # Return calculated fitness value
-    def calc_fitness(self, predicate_num:int, target_predicate_num:int, args:dict):
-        predicate = self.predicates[predicate_num].predicate
+    def calc_fitness(self, func_num:int, target_predicate_num:int, args:dict):
+        def get_args_from_file(current_predicate_num:int):
+            f = open(".execution_log", "r")
+            lines = f.readlines()
+            predicates = list()
+            for line in lines:
+                split_line = line.split('/')
+                predicate_num = int(split_line[0])
+                if predicate_num == current_predicate_num:
+                    f.close()
+                    return ast.literal_eval(split_line[2].strip())
+            f.close()
+            return dict()
+
+        predicate_num = self.get_current_predicate(func_num, args, target_predicate_num)
+        predicate = self.functions[func_num].predicates[predicate_num].predicate
+        args = get_args_from_file(predicate_num)
 
         op = predicate.ops[0]
         left = predicate.left
@@ -239,14 +286,14 @@ class TestGen():
         right_code = astor.to_source(right).rstrip('\n')
 
         for key in args:
-            #print(key + ":" + str(args[key]))
             left_code = left_code.replace(key, str(args[key]))
+            right_code = right_code.replace(key, str(args[key]))
 
         left_eval = eval(left_code)
         right_eval = eval(right_code)
-        # @TODO : calculate approach_level, it could be wrong when we use elif
-        approach_level = len(self.predicates[target_predicate_num].precedent) - len(self.predicates[predicate_num].precedent)
-        
+        # @TODO : calculate approach_level, it could be wrong when we use 'elif'
+        approach_level = len(self.functions[func_num].predicates[target_predicate_num].precedent) - len(self.functions[func_num].predicates[predicate_num].precedent)
+
         branch_distance = 0
         K = self.config["K"]
         if isinstance(op, ast.Gt):
@@ -255,14 +302,20 @@ class TestGen():
             branch_distance = left_eval - right_eval + K
         elif isinstance(op, ast.Eq):
             branch_distance = abs(left_eval - right_eval)
-        #print("branch_distance : " + str(branch_distance))
-        normalized = branch_distance / (branch_distance + 1)#float(1) - pow(1.001, -1 * branch_distance)
-        f = branch_distance#normalized# + approach_level
-
+        # @TODO : Consider <=, >= cases
+        elif isinstance(op, ast.GtE):
+            branch_distance = right_eval - left_eval + K
+        elif isinstance(op, ast.LtE):
+            branch_distance = left_eval - right_eval + K
+        elif isinstance(op, ast.NotEq):
+            branch_distance = -1 * abs(left_eval - right_eval)
+        normalized = 0
+        if branch_distance + 1 != 0:
+            normalized = branch_distance / (branch_distance + 1)
+        f = normalized + approach_level
         return f
-    
+
     # Generate & Execute test code
-    # Returns list of predicates
     def execute_test_suite(self, func_num:int, args:dict):
         # Generate test code
         test_code = ""
@@ -272,6 +325,8 @@ class TestGen():
             test_code = test_code + str(args[arg]) + ", "
         test_code = test_code + ")\n"
 
+        self.init_log_file()
+
         # Execute test file
         exec(compile(test_code, "test", "exec"))
 
@@ -280,22 +335,29 @@ class TestGen():
         lines = f.readlines()
         predicates = list()
         for line in lines:
-            split_line = line.split(" ")
+            split_line = line.split("/")
             predicate_num = int(split_line[0])
             predicates.append(predicate_num)
             option = int(split_line[1])
-        branch_coverage = len(lines) / (len(self.predicates) - 1)
-        #print("branch coverage : " + str(branch_coverage))
+        #branch_coverage = len(lines) / (len(self.predicates) - 1)
         return predicates
+
+    # Print help information
+    def help_function(self):
+        pass
 
 # This is a helper function added to the original code
 # if : 1
 # else : 0
 # elif : 2 ~
-def check_branch(predicate_num, option):
-    #print("num : " + str(predicate_num) + ", option : " + str(option))
+def check_branch(predicate_num, option, char_list, *args):
+    arg_dict = dict()
+    for i in range(len(char_list)):
+        arg_dict[char_list[i]] = args[i]
+
     f = open(".execution_log", "a")
-    f.write(str(predicate_num) + " " + str(option) + "\n")
+    f.write(str(predicate_num) + "/" + str(option) + "/" + str(arg_dict))
+    f.write("\n")
     f.close()
     return
 
--- modified_code.py
+++ modified_code.py
@@ -1,12 +1,35 @@
-import TestGen
+import TestGen2
 
 
-def test_me(x, y, z):
-    if y == 100003:
-        TestGen.check_branch(1, 1)
+def test_me(a, b, c):
+    d = 0
+    if a > b + c:
+        TestGen2.check_branch(1, 0, ['a', 'b', 'c', 'd'], a, b, c, d)
         print('1')
-        z = 1
+        if b != c:
+            TestGen2.check_branch(2, 0, ['a', 'b', 'c', 'd'], a, b, c, d)
+            print('2')
+            d += 1
+        else:
+            TestGen2.check_branch(2, 1, ['a', 'b', 'c', 'd'], a, b, c, d)
+            print('3')
+            d += 2
     else:
-        TestGen.check_branch(1, 0)
-        print('2')
-        x = 2
+        TestGen2.check_branch(1, 1, ['a', 'b', 'c', 'd'], a, b, c, d)
+        print('4')
+        d = d - 1
+    if d > 0:
+        TestGen2.check_branch(3, 0, ['a', 'b', 'c', 'd'], a, b, c, d)
+        print('5')
+        if a > 0:
+            TestGen2.check_branch(4, 0, ['a', 'b', 'c', 'd'], a, b, c, d)
+            print('6')
+            return 1
+        else:
+            TestGen2.check_branch(4, 1, ['a', 'b', 'c', 'd'], a, b, c, d)
+            print('7')
+            return 2
+    else:
+        TestGen2.check_branch(3, 1, ['a', 'b', 'c', 'd'], a, b, c, d)
+        print('8')
+        return 3
